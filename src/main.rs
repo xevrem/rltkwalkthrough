b378461052748a5771a7d7ce27bb5b80787bf569
erika jonell <erika.jonell@gmail.com>
2020-10-30 11:50:56 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use rltk::{GameState, Rltk};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World!");
+    }
+}
+
+fn main() -> rltk::BError {
+    let context = rltk::RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
+}
