3d33792ba0f5ac0ff0e13c551f8c2170b934de75
Erika Jonell <erika.jonell@gmail.com>
2020-11-04 09:05:22 -0500

    some player stuff

--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,39 @@ impl<'a> System<'a> for LeftWalker {
   }
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+  let mut positions = ecs.write_storage::<Position>();
+  let mut players = ecs.write_storage::<Player>();
+
+  for (_player, pos) in (&mut players, &mut positions).join() {
+    pos.x = min(79, max(0, pos.x + delta_x));
+    pos.y = min(49, max(0, pos.y + delta_y));
+  }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+  //player movement
+  match ctx.key {
+    None => {} // nothing happened
+    Some(key) => match key {
+      VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+      VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+      VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+      VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+      _ => {}
+    },
+  }
+}
+
 struct State {
   ecs: World,
 }
 
 impl State {
-  fn run_systems(&mut self){
+  fn run_systems(&mut self) {
     let mut lw = LeftWalker {};
     lw.run_now(&self.ecs);
     self.ecs.maintain();
@@ -58,7 +85,6 @@ impl GameState for State {
     for (pos, render) in (&positions, &renderables).join() {
       ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
     }
-
   }
 }
 
@@ -70,6 +96,7 @@ fn main() -> rltk::BError {
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
   gs.ecs.register::<LeftMover>();
+  gs.ecs.register::<Player>();
 
   gs.ecs
     .create_entity()
@@ -79,6 +106,7 @@ fn main() -> rltk::BError {
       fg: RGB::named(rltk::YELLOW),
       bg: RGB::named(rltk::BLACK),
     })
+    .with(Player {})
     .build();
 
   for i in 0..10 {
